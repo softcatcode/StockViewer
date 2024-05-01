044db576d8915d4861184525f087aaf19bc0daf8
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-01 19:01:02 +0300

    StockPlotState is saved to a data class and rememberSaveable is used instead of remember.

--- app/src/main/java/com/softcat/stockviewer/presentation/stockPlot/stateContent.kt
+++ app/src/main/java/com/softcat/stockviewer/presentation/stockPlot/stateContent.kt
@@ -7,17 +7,16 @@ import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.layout.onSizeChanged
 import com.softcat.stockviewer.domain.entities.Bar
 import kotlin.math.roundToInt
 
@@ -53,40 +52,35 @@ fun DrawScope.drawBar(
 @Composable
 fun BarCanvas(barList: List<Bar>) {
 
-    var visibleBarsCount by remember { mutableIntStateOf(50) }
-    var screenWidth by remember { mutableFloatStateOf(1f) }
-    val barWidth by remember { derivedStateOf { screenWidth / visibleBarsCount } }
-    var scrolledBy by remember { mutableFloatStateOf(0f) }
-
-    val visibleBars by remember {
-        derivedStateOf {
-            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
-            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(barList.size)
-            barList.subList(startIndex, endIndex)
-        }
-    }
+    var stockPlotState by rememberSaveable { mutableStateOf(StockPlotState(barList)) }
 
     val transformableState = TransformableState { zoomChange, panChange, _ ->
-        visibleBarsCount = (visibleBarsCount / zoomChange)
+        val visibleBarCount = (stockPlotState.visibleBarCount / zoomChange)
             .roundToInt()
             .coerceIn(MIN_VISIBLE_COUNT, barList.size)
-        scrolledBy = (scrolledBy + panChange.x)
-            .coerceIn(0f, barWidth * barList.size - screenWidth)
+        val scrolledBy = (stockPlotState.scrolledBy + panChange.x)
+            .coerceIn(0f, stockPlotState.barWidth * barList.size - stockPlotState.screenWidth)
+        stockPlotState = stockPlotState.copy(
+            visibleBarCount = visibleBarCount,
+            scrolledBy = scrolledBy
+        )
     }
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
             .transformable(transformableState)
+            .onSizeChanged {
+                stockPlotState = stockPlotState.copy(screenWidth = it.width.toFloat())
+            }
     ) {
         val minY = 0f
         val maxY = size.height
-        val minPrice = visibleBars.minOf { it.min }
-        val maxPrice = visibleBars.maxOf { it.max }
-        screenWidth = size.width
-        translate(left = scrolledBy) {
+        val minPrice = stockPlotState.visibleBars.minOf { it.min }
+        val maxPrice = stockPlotState.visibleBars.maxOf { it.max }
+        translate(left = stockPlotState.scrolledBy) {
             barList.forEachIndexed { index, bar ->
-                drawBar(minY, maxY, minPrice, maxPrice, index, bar, barWidth)
+                drawBar(minY, maxY, minPrice, maxPrice, index, bar, stockPlotState.barWidth)
             }
         }
     }
