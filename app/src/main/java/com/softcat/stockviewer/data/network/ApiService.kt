11599bd46f67e2512351c4de8b406b7cfc1cd6cf
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-01 12:55:32 +0300

    Loading a reversed bar list.

--- app/src/main/java/com/softcat/stockviewer/data/network/ApiService.kt
+++ app/src/main/java/com/softcat/stockviewer/data/network/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=LYQMRtnPnsiU8hm3vD_szOzby6cXFEBv")
+    @GET("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=LYQMRtnPnsiU8hm3vD_szOzby6cXFEBv")
     suspend fun loadBars(): GetBarsResponseDto
 }
