67f521d27fc788bca8b31df2bb062369840b4b27
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-03 14:40:43 +0300

    Bar count limit was added to loadBars method.

--- app/src/main/java/com/softcat/stockviewer/data/network/ApiService.kt
+++ app/src/main/java/com/softcat/stockviewer/data/network/ApiService.kt
@@ -3,11 +3,13 @@ package com.softcat.stockviewer.data.network
 import com.softcat.stockviewer.data.models.GetBarsResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("https://api.polygon.io/v2/aggs/ticker/AAPL/range/{timeframe}/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=LYQMRtnPnsiU8hm3vD_szOzby6cXFEBv")
+    @GET("https://api.polygon.io/v2/aggs/ticker/AAPL/range/{timeframe}/2022-01-09/2023-01-09?adjusted=true&sort=desc&apiKey=LYQMRtnPnsiU8hm3vD_szOzby6cXFEBv")
     suspend fun loadBars(
-        @Path("timeframe") timeFrame: String
+        @Path("timeframe") timeFrame: String,
+        @Query("limit") limit: Int
     ): GetBarsResponseDto
 }
